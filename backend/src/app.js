c40bc1ae62f6cd32cf06cb7a03ba121cd6e13bce
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-04-06 01:11:40 +0530

    feat: update CORS configuration and serve static files from the dist directory

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,36 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
 import itemRoutes from "./routes/item.route.js";
 import cors from "cors";
-
-
-
-
-
-
+import path from "path";
 
 const app = express();
 
-
-
+// ✅ Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static("./public"));
+
 app.use(cors({
-  origin: "*", // ya origin remove kar do, kyunki same domain se hai
-  credentials: true,
+  origin: "https://t-1-3vkj.onrender.com",
+  credentials: true
 }));
 
-app.use('/api/auth',authRouter);
+
+app.use("/api/auth", authRouter);
 app.use("/api/items", itemRoutes);
 
 
-export default app
+
+const __dirname = path.resolve();
+
+
+app.use(express.static(path.join(__dirname, "backend/dist")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "backend/dist/index.html"));
+});
+
+export default app;
