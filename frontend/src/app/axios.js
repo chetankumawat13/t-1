2f8a0743147085a761be9f21f8a3a50a7f59ef57
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-04-06 00:46:28 +0530

    feat: update API base URL and script references in HTML

--- frontend/src/app/axios.js
+++ frontend/src/app/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://t-1-3vkj.onrender.com/api",
   withCredentials: true, // 🔥 MOST IMPORTANT (cookie send karega)
 });
 
