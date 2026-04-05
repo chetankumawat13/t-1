79d15df489407fd7b9adacbda93d772b5a1bdbe4
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-04-06 00:41:06 +0530

    feat: serve static files from the public directory

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("./public"));
 app.use(
     cors({
       origin: "http://localhost:5173",
